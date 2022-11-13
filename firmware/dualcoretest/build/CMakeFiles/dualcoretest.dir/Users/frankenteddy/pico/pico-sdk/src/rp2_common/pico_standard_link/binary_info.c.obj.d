6cb6296ed5232c59baf52068d8adde5e17302e3c
RobotGrrl <erin@robotgrrl.com>
2022-11-13 15:07:00 -0500

    Added dual core test

---
+++ firmware/dualcoretest/build/CMakeFiles/dualcoretest.dir/Users/frankenteddy/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj.d
@@ -0,0 +1,37 @@
+CMakeFiles/dualcoretest.dir/Users/frankenteddy/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj: \
+ /Users/frankenteddy/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/defs.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/structure.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/stdint.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/features.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_stdint.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/code.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/assert.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/_ansi.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/newlib.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/config.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /Users/frankenteddy/Documents/PlatformIO/ButterflyWatch/dualcoretest/build/generated/pico_base/pico/version.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /Users/frankenteddy/Documents/PlatformIO/ButterflyWatch/dualcoretest/build/generated/pico_base/pico/config_autogen.h \
+ /Users/frankenteddy/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /Users/frankenteddy/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /Users/frankenteddy/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /Users/frankenteddy/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /Users/frankenteddy/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /Users/frankenteddy/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/cdefs.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /Users/frankenteddy/pico/pico-sdk/src/rp2_common/boot_stage2/include/boot_stage2/config.h \
+ /Users/frankenteddy/pico/pico-sdk/src/common/pico_base/include/pico/config.h
---
+++ firmware/dualcoretest/build/CMakeFiles/dualcoretest.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dualcoretest.
+# This may be replaced when dependencies are built.
