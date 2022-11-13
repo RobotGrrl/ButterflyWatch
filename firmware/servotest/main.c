a79db3ce74e619a144c5ec0538f0b51946379c69
RobotGrrl <erin@robotgrrl.com>
2022-11-13 16:27:09 -0500

    Double servos moving

--- firmware/servotest/main.c
+++ firmware/servotest/main.c
@@ -4,35 +4,58 @@
 #include "pico/stdlib.h"
 #include "servo.h"
 
-bool direction = true;
-int currentMillis = 400;
-int servoPin = 7;
+const int min = 600;
+const int max = 2400;
+
+bool direction1 = true;
+bool direction2 = false;
+int currentMillis1 = min;
+int currentMillis2 = max;
+int servoPin1 = 7;
+int servoPin2 = 6;
 
 int main()
 {
+
+    stdio_init_all();
 
     const uint led_pin = 25;
 
     gpio_init(led_pin);
     gpio_set_dir(led_pin, GPIO_OUT);
 
-    stdio_init_all();
+    
+    setServo(servoPin1, currentMillis1);
+    setServo(servoPin2, currentMillis2);
 
-
-    setServo(servoPin, currentMillis);
     while (true)
     {
-        currentMillis += (direction)?5:-5;
-        if (currentMillis >= 2400) {
-            direction = false;
+        currentMillis1 += (direction1)?5:-5;
+        currentMillis2 += (direction2)?5:-5;
+
+        setMillis(servoPin1, currentMillis1);
+        setMillis(servoPin2, currentMillis2);
+
+        printf("1: %d 2: %d\r\n", currentMillis1, currentMillis2);
+
+        sleep_ms(10);
+
+        if (currentMillis1 >= max) {
+            direction1 = false;
             gpio_put(led_pin, false);
         }
-        if (currentMillis <= 400) {
-            direction = true;
+        if (currentMillis1 <= min) {
+            direction1 = true;
             gpio_put(led_pin, true);
         }
-        setMillis(servoPin, currentMillis);
-        sleep_ms(10);
+
+        if (currentMillis2 >= max) {
+            direction2 = false;
+        }
+        if (currentMillis2 <= min) {
+            direction2 = true;
+        }
+
     }
 }
 
